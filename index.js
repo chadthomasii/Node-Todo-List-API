d4333e615590f753aae2aef77e3bf65b2e3924e2
chad <chad.thomas.ii@gmail.com>
2018-05-23 11:18:17 -0400

    Finished Create, Read, Update, and Delete functionality of API

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+//Imported routes
 var express = require('express');
 app = express();
 var port = 3000;
+var bodyParser = require('body-parser');
+
 var todoRoutes = require('./routes/todos')
 
+//Used to access the body of the request. Added now to the App object
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api/todos', todoRoutes); //Setup routes in routes folder
 
 
-app.use('/api/todos', todoRoutes);
 
 
-
-
-app.listen(port);
+app.listen(port); //Accept traffic on stated port
--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var db = require("../models"); //Require Database
-var bodyParser = require('body-parser');
 
+//Default route send back a list of all the todos.
 router.get("/", function(request,resend)
 {
     db.Todo.find()
@@ -10,18 +10,76 @@ router.get("/", function(request,resend)
     {
         resend.json(todos);
     })
-    .catch(function(error)
+    .catch(function(error) //error if there is one
     {
         resend.send(error);
     });
 
 
 });
-
+//Add new Todo
 router.post('/', function (request, resend)
 {
-    resend.send('This is the post route');
+    //Grab the new json object and stick it in the Database
+    db.Todo.create(request.body)
+    .then(function(newTodo)
+    {   
+        resend.status(201).json(newTodo); //Send the new Object back
+        //Status 201 means something was created
+    })
+    .catch(function(error)
+    {
+        resend.send(error); //Send the error of there is one
+    });
 });
+
+//Get Todo with ID
+router.get('/:todoId', function(request,resend)
+{
+    //Sends back a todo object when given ID
+    db.Todo.findById(request.params.todoId)
+    .then(function(foundTodo)
+    {
+        resend.json(foundTodo);
+    })
+    .catch(function(error)
+    {
+        resend.send(error);
+    });
+});
+
+//Updates the selected Task, pass an ID
+router.put('/:todoId', function(request,response)
+{
+    //New keyword send back the updated string on request
+    db.Todo.findOneAndUpdate({_id: request.params.todoId}, request.body, {new: true})
+    .then(function(todo)
+    {
+        response.json(todo);
+    })
+    .catch(function(error)
+
+    {
+        response.send(error);
+    });
+
+});
+
+//Delete the Selected Task, pass an ID
+router.delete('/:todoId', function(request,response)
+{
+    db.Todo.remove({_id: request.params.todoId})
+    .then(function()
+    {
+        response.json({message: "Task Deleted"});
+    })
+    .catch(function(error)
+    {
+        response.send(error);
+    });
+});
+
+
 
 
 module.exports = router;
