ea33cd200198c0f231c55afa01b59dbc3c9de7cf
Chad Thomas II <chad@skylar.gateway.pace.com>
2019-01-16 12:43:11 -0500

    Added gitigonre again

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-//Imported routes
-var express = require('express');
-app = express();
-var port = 3000;
-var bodyParser = require('body-parser');
+var express = require('express'), app = express();
 
-var todoRoutes = require('./routes/todos')
-
-//Used to access the body of the request. Added now to the App object
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use('/api/todos', todoRoutes); //Setup routes in routes folder
-
-
-
-
-app.listen(port); //Accept traffic on stated port
