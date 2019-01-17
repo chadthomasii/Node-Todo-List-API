3db7553558facd0ec1ef9c20f0d6269c26260bf8
Chad Thomas II <chad@skylar.gateway.pace.com>
2019-01-17 13:39:10 -0500

    Added more files to ignore on project.

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-var express = require('express'), app = express();
+var app = require('express')();
+const PORT = 80;
+var bodyParser = require('body-parser');
+
+var todoRoutes = require('./routes/todos');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(
+    {
+        extended: true
+    }
+));
+
+app.get('/', function(req,res){
+    res.send("Hello from the root route")
+})
+
+app.use('/api/todos', todoRoutes);
+
+app.listen(PORT, function(){
+    console.log('Listening on port ' + PORT); //Listening on port 8888
+});
+
 
