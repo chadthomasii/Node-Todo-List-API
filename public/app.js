b2ec1cc6bfda0f4f0a3acb59f83bc7c49f2187e6
Chad Thomas II <chad@skylar.gateway.pace.com>
2019-01-18 21:03:33 -0500

    Todos are now able to be deleted.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,78 @@
 $(document).ready(function()
 {
 
+    $.getJSON('/api/todos').then(addTodos);
+    
+    $('#todoInput').keypress(function(event)    
+    {   
+        if(event.which == 13)
+        {
+            createTodo();
+        }
+    });
+
+    $('.list').on('click', 'span', function(e)
+    {
+        e.stopPropagation();
+        removeTodo($(this).parent());
+    })
+
+    
 });
+
+function addTodos(todos) 
+{
+	//add todos here
+    todos.forEach(function(todo) 
+    {
+        addTodo(todo);
+	});
+}
+
+function addTodo(todo)
+{
+    var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
+
+    newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
+    if(todo.completed)
+    {
+        newTodo.addClass("done");
+    }
+    $('.list').append(newTodo);
+}
+
+function createTodo()
+{
+    //send request to create todo
+
+    var userInput = $('#todoInput').val();
+    $.post('/api/todos', {name: userInput })
+    .then(function(newTodo)
+    {
+        $('#todoInput').val('');
+        addTodo(newTodo);
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+}
+
+function removeTodo(todo)
+{
+    var clickedId = todo.data('id');
+    var deleteUrl = '/api/todos/' + clickedId; 
+    $.ajax
+    ({
+        method: 'DELETE',
+        url: deleteUrl
+    })
+    .then(function(data)
+    {
+        todo.remove();
+    })
+    .catch(function(err)
+    {
+        console.log(err);
+    })
+}
